162db9fcb5590a1602e5bb5170cccfdd03798d8c
amsrich97 <amsrich97@gmail.com>
2022-04-22 15:02:12 +1000

    excel data test 1st change

--- src/test/java/ExcelDataTest.java
+++ src/test/java/ExcelDataTest.java
@@ -6,15 +6,15 @@ public class ExcelDataTest {
     public static void main(String[] args) throws IOException {
         // TODO Auto-generated method stub
 
-
-        DataDriven d=new DataDriven();
-        ArrayList data=d.getData("RestAssured");
-
-       // System.out.println(data.get(0));
-        System.out.println(data.get(1));
-        System.out.println(data.get(2));
-        System.out.println(data.get(3));
-        System.out.println(data.get(4));
+System.out.println("Git changes 1");
+//        DataDriven d=new DataDriven();
+//        ArrayList data=d.getData("RestAssured");
+//
+//       // System.out.println(data.get(0));
+//        System.out.println(data.get(1));
+//        System.out.println(data.get(2));
+//        System.out.println(data.get(3));
+//        System.out.println(data.get(4));
 
 
 
